a3feb1154b3b5eb2457a7b775c6c686f092d37d8
ziv ankri <za6200@bs.amalnet.k12.il>
2023-11-20 12:44:42 +0200

    added back button in credits

--- app/src/main/java/com/example/ex32year2/credits.java
+++ app/src/main/java/com/example/ex32year2/credits.java
@@ -3,7 +3,9 @@ package com.example.ex32year2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class credits extends AppCompatActivity {
@@ -14,11 +16,17 @@ public class credits extends AppCompatActivity {
      * class will give the credit to Albert Levi
      */
     TextView textView;
+    Intent backActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
         textView = findViewById(R.id.textView2);
         textView.setText("credit to Albert Levi");
+        backActivity = new Intent(this, MainActivity.class);
+    }
+
+    public void back(View view) {
+        startActivity(backActivity);
     }
 }
